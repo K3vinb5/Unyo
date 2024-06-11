7219234f1a5f0bc17cab39e87a4be2ed7bdad3dc
Kevin Rodrigues Borges <115191440+K3vinb5@users.noreply.github.com>
2024-06-11 21:05:34 +0100

    Test commit (Should be ignored)

--- lib/screens/scaffold_screen.dart
+++ lib/screens/scaffold_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:unyo/widgets/widgets.dart';
 
 late StatefulNavigationShell publicNavigationShell;
 
