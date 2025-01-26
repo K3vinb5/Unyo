93393717cab6d6b26f69264ec5f185dce326c119
Kevin Rodrigues Borges <115191440+K3vinb5@users.noreply.github.com>
2025-01-26 15:41:31 +0000

    (fix) initializing 2 times

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,6 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Window.initialize();
   await EasyLocalization.ensureInitialized();
-  WidgetsFlutterBinding.ensureInitialized();
   final dir = await getApplicationSupportDirectory();
   Hive.init(p.join(dir.path, "data"));
   Hive.registerAdapter(AnilistUserModelAdapter());
